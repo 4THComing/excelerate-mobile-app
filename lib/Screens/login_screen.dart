742db0a9f8223fabe80c8dacf8ecf21b2a61ae6d
Zolani Mjikeliso <zolanimjikeliso20@gmail.com>
2026-06-13 21:49:30 +0200

    Create login screen UI

---
+++ lib/Screens/login_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login"), centerTitle: true),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 80,
+              height: 50,
+              decoration: BoxDecoration(border: Border.all()),
+              child: const Center(child: Text("Logo")),
+            ),
+
+            const SizedBox(height: 25),
+
+            TextField(
+              decoration: const InputDecoration(
+                hintText: "Email",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            TextField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: "Password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            ElevatedButton(onPressed: () {}, child: const Text("Login")),
+
+            const SizedBox(height: 15),
+
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Forgot Password"),
+            ),
+
+            const SizedBox(height: 15),
+
+            const Text("Don't have an account?"),
+
+            TextButton(onPressed: () {}, child: const Text("Register")),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Excelerate',
-      home: Scaffold(appBar: AppBar(title: const Text('Excelerate'))),
+      home: const LoginScreen(),
     );
   }
 }
